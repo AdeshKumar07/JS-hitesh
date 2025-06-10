8815139e4ef92fc06568194599c6eb9768acac59
Adesh Kumar <adeshak0707@gmail.com>
2025-06-10 15:32:28 +0000

    Varibales declaration

---
+++ variables.js
@@ -0,0 +1,11 @@
+const accountId= 12345 //method of declartion of const
+//method of declartion of varialble
+let accountEmail="adesh@123.gmail.com"  //  changeable 
+var accountPassword="12345"//forgot it 
+let accountState;
+accountCity="Jaipur"
+accountEmail="Sunnny@.gmail.com"
+accountPassword="6789"
+accountCity="Luckhnow"
+console.log()
+console.table([accountId,accountEmail,accountPassword,accountCity,accountState])
